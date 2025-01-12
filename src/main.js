2bc19f52531cd4e1819c08aadbaf65977b7549f7
thaivanquy <thaivanquy2503@gmail.com>
2025-01-13 06:59:40 +0700

    feat: add file env and && edit router

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import "normalize.css";
 import "./assets/styles/global.css";
-import router from "./router";
+import router from "./router/index";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import store from "./store";
 import Tabs from "element-ui";
 import "element-ui/lib/theme-chalk/tabs.css";
+import "video.js/dist/video-js.css";
 
 Vue.config.productionTip = false;
 Vue.use(Tabs);
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,72 @@
 import Vue from "vue";
 import Router from "vue-router";
 import HomeView from "../views/Home.vue";
+import MovieDetailView from "../views/MovieDetail.vue";
+import BrowseView from "../views/Browse.vue";
+import SeriesView from "../views/Series.vue";
+import SingleView from "../views/Single.vue";
+import NewView from "../views/New.vue";
+import SearchView from "../views/Search.vue";
+import FaqView from "../views/Faq.vue";
+import TopView from "../views/Top.vue";
+import WatchView from "../views/Watch.vue";
 
 Vue.use(Router);
 
-const routes = [{ path: "/", name: "HomeView", component: HomeView }];
-
 const router = new Router({
   mode: "history",
-  routes,
+  routes: [
+    {
+      path: "/",
+      name: "HomeView",
+      component: HomeView,
+    },
+    {
+      path: "/movie/:slug",
+      name: "MovieDetailView",
+      component: MovieDetailView,
+    },
+    {
+      path: "/browse",
+      name: "BrowseView",
+      component: BrowseView,
+    },
+    {
+      path: "/series",
+      name: "SeriesView",
+      component: SeriesView,
+    },
+    {
+      path: "/single",
+      name: "SingleView",
+      component: SingleView,
+    },
+    {
+      path: "/new",
+      name: "NewView",
+      component: NewView,
+    },
+    {
+      path: "/seach",
+      name: "SearchView",
+      component: SearchView,
+    },
+    {
+      path: "/faqs",
+      name: "FaqView",
+      component: FaqView,
+    },
+    {
+      path: "/top",
+      name: "TopView",
+      component: TopView,
+    },
+    {
+      path: "/watch/:slug",
+      name: "WatchView",
+      component: WatchView,
+    },
+  ],
 });
 
 export default router;
