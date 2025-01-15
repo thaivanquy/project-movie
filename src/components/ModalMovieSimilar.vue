080cc14c38294762d63e5648aae50dbaa6f0ae3e
thaivanquy <thaivanquy2503@gmail.com>
2025-01-15 15:28:06 +0700

    feat: fix error watch

---
+++ src/components/ModalMovieSimilar.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="modal__overlay">
+    <div class="modal__container">
+      <div class="modal__head">
+        <h1>Phim tương tự</h1>
+        <div class="btn-close">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z"></path></svg>
+        </div>
+      </div>
+      <div class="modal__content">
+        <MovieComponent v-for="movie in movies" :key="movie._id" :thumbUrl="movie.thumb_url" :nameVi="movie.name" :nameEn="movie.origin_name" :slug="movie.slug"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MovieComponent from '../components/Movie.vue';
+export default {
+  name: 'ModalMovieSimilarComponent',
+  components: {
+    MovieComponent
+  },
+  data() {
+    return {
+      movies: [
+        {
+          _id: 1,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        },
+        {
+          _id: 22,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        },        {
+          _id: 33,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        },        {
+          _id: 44,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        },        {
+          _id: 55,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        },        {
+          _id: 66,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        },        {
+          _id: 77,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        },        {
+          _id: 88,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        },        {
+          _id: 99,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        },        {
+          _id: 11,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        }, {
+          _id: 12,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        }, {
+          _id: 13,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        }, {
+          _id: 14,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        }, {
+          _id: 15,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        }, {
+          _id: 16,
+          thumb_url: 'solo-leveling-phan-2-vung-len-tu-bong-toi-tbc-thumb.jpg',
+          name: 'Test',
+          origin_name: 'Test',
+          slug: 'song-nay-biet-mai'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scope>
+.modal__overlay {
+  display: none !important;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+}
+
+.modal__container {
+  background-color: #0b1e2f;
+  width: 100%;
+  height: 90%;
+  max-width: 1280px;
+  color: #fff;
+  overflow: hidden;
+}
+
+.modal__head {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 24px;
+  border-bottom: 1px solid #273a4b;
+}
+
+.modal__head h1 {
+  font-size: 24px !important;
+  line-height: 36px;
+  font-weight: 700;
+  color: rgb(255, 255, 255);
+}
+
+.modal__content {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 16px;
+  overflow-y: auto;
+  padding: 16px 24px;
+  max-height: calc(100% - 64px);
+  box-sizing: border-box; 
+}
+
+.btn-close {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  --bs-btn-close-opacity: 1 !important;
+}
+
+.btn-close svg {
+  fill : #fff;
+}
+
+</style>
